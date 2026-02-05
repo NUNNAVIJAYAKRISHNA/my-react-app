2f114f39a006618a3ef66a22c1e4549d73d36eb4
NUNNAVIJAYAKRISHNA <vijayakrishna.n213@gmail.com>
2026-02-05 15:46:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ProfileCard } from "./components/profileCard"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <ProfileCard />
+  )
+}
+
+export default App
---
+++ src/components/profileCard.jsx
@@ -0,0 +1,19 @@
+import profilePic from '../assets/images/profile_picture.jpg'
+export function ProfileCard() {
+  return (
+    <div className="bg-white flex flex-col md:flex-row items-center gap-4 p-6 rounded-xl w-fit border-l-8 border-orange-500 shadow-md m-2">
+      <img
+        src={profilePic}
+        alt="Profile"
+        className="rounded-full h-40 w-40 object-cover"
+      />
+      <div className="flex flex-col items-center md:items-start text-center md:text-left gap-1">
+        <p className="text-3xl font-semibold">NUNNA VIJAYA KRISHNA</p>
+        <p className="text-xl">Computer Science and Engineering</p>
+        <p className="text-xl">Aditya Institute of Technology and Management</p>
+        <p className="text-lg">CGPA: 8.71 | Expected Graduation: 2027</p>
+      </div>
+    </div>
+
+  );
+}
